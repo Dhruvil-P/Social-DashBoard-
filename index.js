24e94f9c2f1528230c9873ee19076933b7d51241
Dhruvil Patel <76477111+Dhruvil-P@users.noreply.github.com>
2021-01-12 00:49:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    ($(document).ready(function(){
+        $(".Slider").on('click', function(){
+            $(".Slider").toggleClass("Slider-Toggle")
+            $(".Toggle").toggleClass("Switch-Toggle")
+            $(".Dash").toggleClass("Dash-Toggle")
+            $("*").toggleClass("M")
+
+            $(".FB").toggleClass("FB-Toggle")
+            $(".Twitter").toggleClass("Twitter-Toggle")
+            $(".Insta").toggleClass("Insta-Toggle")
+            $(".YT").toggleClass("YT-Toggle")
+
+            $("h2").toggleClass("h2-Toggle")
+            $("h5").toggleClass("h5-Toggle")
+            $(".Mode").toggleClass("Mode-Toggle")
+            $(".Name").toggleClass("Name-Toggle")
+            $(".Span").toggleClass("Span-Toggle")
+
+            $(".Num1").toggleClass("Num1-Toggle")
+            $(".Num2").toggleClass("Num2-Toggle")
+            $(".Num3").toggleClass("Num3-Toggle")
+            $(".Num4").toggleClass("Num4-Toggle")
+
+            $(".P1").toggleClass("P1-Toggle")
+            $(".P2").toggleClass("P2-Toggle")
+            $(".P3").toggleClass("P3-Toggle")
+            $(".P4").toggleClass("P4-Toggle")
+
+            $(".P5").toggleClass("P5-Toggle")
+            $(".P6").toggleClass("P6-Toggle")
+            $(".P7").toggleClass("P7-Toggle")
+            $(".P8").toggleClass("P8-Toggle")
+
+            $(".Head").toggleClass("Head-Toggle")
+            $(".num").toggleClass("num-Toggle")
+
+            $(".attribution").toggleClass("attribution-Toggle")
+
+        })
+    }))
+})
